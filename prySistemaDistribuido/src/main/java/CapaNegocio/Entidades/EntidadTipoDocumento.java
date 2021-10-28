1ca6596ce83bfc78ba90adff3a5a243d4f5156c0
rodri <rodri@DESKTOP-8A42J54>
2021-10-28 08:07:38 -0500

    Se agrego los atributos y metodos a la entidad tipodocumento

--- prySistemaDistribuido/src/main/java/CapaNegocio/Entidades/EntidadTipoDocumento.java
+++ prySistemaDistribuido/src/main/java/CapaNegocio/Entidades/EntidadTipoDocumento.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package CapaNegocio.Entidades;
 
 /**
@@ -10,5 +5,38 @@ package CapaNegocio.Entidades;
  * @author Rodrigo Ruidias
  */
 public class EntidadTipoDocumento {
+    
+    private int tipo_documento_id;
+    private String nombre;
+    
+    public EntidadTipoDocumento(){
+        
+    }
+
+    public EntidadTipoDocumento(int tipo_documento_id, String nombre) {
+        this.tipo_documento_id = tipo_documento_id;
+        this.nombre = nombre;
+    }
+
+    public int getTipo_documento_id() {
+        return tipo_documento_id;
+    }
+
+    public void setTipo_documento_id(int tipo_documento_id) {
+        this.tipo_documento_id = tipo_documento_id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    @Override
+    public String toString(){
+        return nombre;
+    }
     
 }
